5cae5eccd328e1ede1cadff4dbccb83bac3089ff
Emilio Latorre <64216678+elatorre20@users.noreply.github.com>
2020-05-09 15:47:07 -0400

    work on player model, added mouselook

---
+++ fpsgame/Assets/Scripts/Player/MouseLook.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    //set mouse sensitivity
+    public float mouseSensitivity = 100f;
+    //reference alignment to player body
+    public Transform playerBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get mouse x (horizontal)
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        //get mouse y (vertical
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity* Time.deltaTime;
+
+        //rotate camera
+        playerBody.Rotate(Vector3.up * mouseX);
+
+    }
+}
