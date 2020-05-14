9dd18aa0a4defb9d22c246868d420c9340a004fb
Emilio Latorre <64216678+elatorre20@users.noreply.github.com>
2020-05-13 22:46:11 -0400

    cleaned up and reverted to old input system
    noticeable performance increase and also vastly simpler coding when using the old input system

---
+++ FPSgame/Assets/Scripts/Player/MouseLook.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float MouseSensitivity = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float MouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
+        float MouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+
+    }
+}
--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
 Authors: Emilio Latorre and Alex Rabino
 Name: TBD
-Version: 0.0.1
-Date: 5/4/20
+Version: 0.0.2
+Date: 5/12/20
